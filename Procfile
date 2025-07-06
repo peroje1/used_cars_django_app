4fe27b1f23e523b3db4c0d54618dc9ba2234fc33
Petar Nikic <petarnikic@rocketmail.com>
2025-07-06 17:42:50 +0200

    Prepare for Render deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn used_cars_web.config.wsgi
--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3arlmo9ckzy(an-1oac@8c1=rtq98mioz6dyq@*b3ew9v1(l9u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
